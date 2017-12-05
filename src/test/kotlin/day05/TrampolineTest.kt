98be9378d572e7f9f2dd9964470516967888a209
Andrey Shcherbakov <veontomo@gmail.com>
2017-12-05 20:43:17 +0100

    Resolve the second part of day 5 problem

--- src/test/kotlin/day05/TrampolineTest.kt
+++ src/test/kotlin/day05/TrampolineTest.kt
@@ -7,8 +7,13 @@ import org.junit.Assert.*
 class TrampolineTest {
 
     @Test
-    fun `0 3 0 1 -3 returns 5`() {
+    fun `0 3 0 1 -3 falls out in five jumps`() {
         val c = Trampoline()
         assertEquals(5, c.move(arrayOf(0, 3, 0, 1, -3),0, 0))
+    }
+    @Test
+    fun `0 3 0 1 -3 falls out in 10 strange jumps`() {
+        val c = Trampoline()
+        assertEquals(10, c.moveStrange(arrayOf(0, 3, 0, 1, -3),0, 0))
     }
 }
