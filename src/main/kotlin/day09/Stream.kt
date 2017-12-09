0b4dad816c0d3f3a6c7596629c1f8e3541b73e46
Andrey Shcherbakov <veontomo@gmail.com>
2017-12-09 20:48:54 +0100

    Resolve the second part of day 09 problem

--- src/main/kotlin/day09/Stream.kt
+++ src/main/kotlin/day09/Stream.kt
@@ -3,33 +3,47 @@ package day09
 import Answer
 
 class Stream : Answer {
+
+
     override fun showAnswer(): String {
         val input = readLines("day09.txt")
-        return score(input[0]).toString()
+        val result = score(input[0])
+        return "score: ${result.first}, garbage length: ${result.second}"
     }
 
-    fun score(input: String): Int {
-        return process(input, 0, 0, 0, 0, false)
+    fun score(input: String): Pair<Int, Int> {
+        return process(input, 0, 0, 0, 0, false, 0)
     }
 
-    tailrec private fun process(input: String, pos: Int, depth: Int, totalScore: Int, prevGroupScore: Int, isGarbage: Boolean): Int {
+    tailrec private fun process(input: String,
+                                pos: Int,
+                                depth: Int,
+                                totalScore: Int,
+                                prevGroupScore: Int,
+                                isGarbage: Boolean,
+                                garbageLength: Int
+    ): Pair<Int, Int> {
         val s = input.length
         if (pos >= s) {
             check(depth == 0, { "unexpected end of input. Unclosed group are present." })
-            return totalScore
+            return Pair(totalScore, garbageLength)
         }
         val character = input[pos]
         if (isGarbage) {
-            val jump = if(character == '!') 2 else 1
-            return process(input, pos + jump, depth, totalScore, prevGroupScore, character != '>')
+            val isGarbageEnd = character == '>'
+            val shouldJump = character == '!'
+            val jump = if (shouldJump) 2 else 1
+            val count = if (shouldJump || isGarbageEnd) 0 else 1
+
+            return process(input, pos + jump, depth, totalScore, prevGroupScore, !isGarbageEnd, garbageLength + count)
         }
-        assert(!isGarbage, {"should be not garbage at this point"})
+        assert(!isGarbage, { "should be not garbage at this point" })
         return when (character) {
-            '{' -> process(input, pos + 1, depth + 1, totalScore, prevGroupScore + 1, isGarbage)
-            '}' -> process(input, pos + 1, depth - 1, totalScore + prevGroupScore, prevGroupScore - 1, isGarbage)
-            '<' -> process(input, pos + 1, depth, totalScore, prevGroupScore, true)
-            '!' -> process(input, pos + 2, depth, totalScore, prevGroupScore, isGarbage)
-            else -> process(input, pos + 1, depth, totalScore, prevGroupScore, isGarbage)
+            '{' -> process(input, pos + 1, depth + 1, totalScore, prevGroupScore + 1, isGarbage, garbageLength)
+            '}' -> process(input, pos + 1, depth - 1, totalScore + prevGroupScore, prevGroupScore - 1, isGarbage, garbageLength)
+            '<' -> process(input, pos + 1, depth, totalScore, prevGroupScore, true, garbageLength)
+            '!' -> process(input, pos + 1, depth, totalScore, prevGroupScore, isGarbage, garbageLength)
+            else -> process(input, pos + 1, depth, totalScore, prevGroupScore, isGarbage, garbageLength)
         }
 
     }
--- src/test/kotlin/day09/StreamTest.kt
+++ src/test/kotlin/day09/StreamTest.kt
@@ -8,53 +8,98 @@ class StreamTest() {
     @Test
     fun `{}, score of 1`() {
         val c = Stream()
-        assertEquals(1, c.score("{}"))
+        assertEquals(1, c.score("{}").first)
     }
 
     @Test
     fun `{{{}}}, score of 1 + 2 + 3 = 6`() {
         val c = Stream()
-        assertEquals(6, c.score("{{{}}}"))
+        assertEquals(6, c.score("{{{}}}").first)
     }
 
     @Test
     fun `{{},{}}, score of 1 + 2 + 2 = 5`() {
         val c = Stream()
-        assertEquals(5, c.score("{{}, {}}"))
+        assertEquals(5, c.score("{{}, {}}").first)
     }
 
     @Test
     fun `{{{},{},{{}}}}, score of 1 + 2 + 3 + 3 + 3 + 4 = 16`() {
         val c = Stream()
-        assertEquals(16, c.score("{{{},{},{{}}}}"))
+        assertEquals(16, c.score("{{{},{},{{}}}}").first)
     }
 
     @Test
     fun `single group with garbage`() {
         val input = "{<a>,<a>,<a>,<a>}"
         val c = Stream()
-        assertEquals(1, c.score(input))
+        assertEquals(1, c.score(input).first)
     }
 
     @Test
     fun `group with four nested  garbages`() {
         val input = "{{<ab>},{<ab>},{<ab>},{<ab>}}"
         val c = Stream()
-        assertEquals(9, c.score(input))
+        assertEquals(9, c.score(input).first)
     }
 
     @Test
     fun `group with comments`() {
         val input = "{{<!!>},{<!!>},{<!!>},{<!!>}}"
         val c = Stream()
-        assertEquals(9, c.score(input))
-
+        assertEquals(9, c.score(input).first)
     }
 
     @Test
     fun `group with comments 2`() {
         val input = "{{<a!>},{<a!>},{<a!>},{<ab>}}"
         val c = Stream()
-        assertEquals(3, c.score(input))
+        assertEquals(3, c.score(input).first)
     }
+
+    @Test
+    fun emptyAngleBrackets() {
+        val input = "<>"
+        val c = Stream()
+        assertEquals(0, c.score(input).second)
+    }
+
+    @Test
+    fun expressionInAngleBrackets() {
+        val input = "<random characters>"
+        val c = Stream()
+        assertEquals(17, c.score(input).second)
+    }
+
+    @Test
+    fun angleBrackets() {
+        val input = "<<<<>"
+        val c = Stream()
+        assertEquals(3, c.score(input).second)
+    }
+
+    @Test
+    fun singleJump() {
+        val c = Stream()
+        assertEquals(2, c.score("<{!>}>").second)
+    }
+
+    @Test
+    fun twoJumps() {
+        val c = Stream()
+        assertEquals(0, c.score("<!!>").second)
+    }
+
+    @Test
+    fun doubleJumps() {
+        val c = Stream()
+        assertEquals(0, c.score("<!!!>>").second)
+    }
+
+    @Test
+    fun realistic() {
+        val c = Stream()
+        assertEquals(10, c.score("<{o\"i!a,<{i<a>").second)
+    }
+
 }
