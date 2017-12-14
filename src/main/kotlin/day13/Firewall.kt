cde529c1e9fb33ba17f455eecbb2e8c97b48f64f
Andrey Shcherbakov <veontomo@gmail.com>
2017-12-14 22:09:09 +0100

    Resolve part 2 of day 13

--- src/main/kotlin/day13/Firewall.kt
+++ src/main/kotlin/day13/Firewall.kt
@@ -8,14 +8,33 @@ class Firewall : Answer {
                 .fold(mapOf<Int, Int>(), { acc, v -> acc + Pair(v[0], v[1]) })
         val firewallsNum = input.keys.max()!! + 1
         val data = IntArray(firewallsNum, { index -> if (input.containsKey(index)) input[index]!! else 0 })
-        val result = calculate(data)
-        return result.toString()
+        val severity = calculate(data)
+        val minDelay = findOffset(data)
+        return "severity: $severity, min delay: $minDelay"
     }
 
-    fun calculate(data: IntArray): Int {
-        val tmp = data.mapIndexed { i, v -> if (v >= 0 && (i % (2 * v - 2) == 0)) i * v else 0 }
-        print(tmp)
-        return tmp.sum()
+    fun calculate(data: IntArray, delay: Int = 0): Int {
+        val m = mask(data, delay)
+        return data.mapIndexed { index, value -> if (m[index]) index * value else 0 }.sum()
+//        val tmp = data.mapIndexed { i, v -> if (v >= 0 && ((i + delay) % (2 * v - 2) == 0)) i * v else 0 }
+        //      return tmp.sum()
 
+    }
+
+    /**
+     * Return array of booleans: if at cell n you get caught, then the n-th value if true, otherwise - false
+     */
+    fun mask(data: IntArray, delay: Int = 0): BooleanArray {
+        return data.mapIndexed { index, value -> value > 0 && ((index + delay) % (2 * value - 2) == 0) }.toBooleanArray()
+
+    }
+
+
+    fun findOffset(data: IntArray): Int {
+        var offset = 0
+        while (mask(data, offset).any { it }) {
+            offset++
+        }
+        return offset
     }
 }
--- src/test/kotlin/day13/FirewallTest.kt
+++ src/test/kotlin/day13/FirewallTest.kt
@@ -12,4 +12,10 @@ class FirewallTest {
         assertEquals(24, c.calculate(intArrayOf(3, 2, 0, 0, 4, 0, 4)))
 
     }
+
+    @Test
+    fun findOffset() {
+        val c = Firewall()
+        assertEquals(0, c.calculate(intArrayOf(3, 2, 0, 0, 4, 0, 4), 10))
+    }
 }
